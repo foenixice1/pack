2fb320f5c97dbab9f7c52105a93496aeaf3765d4
foenixice1 <foenixice1@gmail.com>
2021-06-13 21:51:34 +0700

    thuchanh

---
+++ src/TestCircle.java
@@ -0,0 +1,10 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(5.0);
+        circle.getRadius();
+        circle.setColor("blue");
+        circle.getArea();
+
+    }
+}
